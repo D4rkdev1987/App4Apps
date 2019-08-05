577240449491497588e733e1f0bb87f17e0e09c9
Benjamin Nicklaus <Marineepo87@gmail.com>
2019-08-05 19:34:36 -0400

    second push

---
+++ src/non-components/cards.js
@@ -0,0 +1,79 @@
+export const CARDS = [
+    {
+      id: 0,
+      name: "Ben's App",
+      type: "New_apps",
+      image: "images/BottegaTeam.png",
+      description: "Schedule"
+    },
+    {
+      id: 1,
+      name: "Alex",
+      type: "New_apps",
+      image: "images/BottegaTeam.png",
+      description: "Bottega site"
+    },
+    {
+      id: 2,
+      name: "Makenna",
+      type: "working_on",
+      image: "images/BottegaTeam.png",
+      description: "Another app"
+    },
+    {
+      id: 3,
+      name: "Chantay",
+      type: "New_apps",
+      image: "images/BottegaTeam.png",
+      description: "Schedule app"
+    },
+    {
+      id: 4,
+      name: "Bobby",
+      type: "working_on",
+      image: "images/BottegaTeam.png",
+      description: "Scheduler app"
+    },
+    {
+      id: 5,
+      name: "John",
+      type: "finished_apps",
+      image: "images/BottegaTeam.png",
+      description: "Bottega html"
+    },
+    {
+      id: 6,
+      name: "Jackson",
+      type: "working_on",
+      image: "images/BottegaTeam.png",
+      description: "Application for apps"
+    },
+    {
+      id: 7,
+      name: "Ben",
+      type: "finished_apps",
+      image: "images/BottegaTeam.png",
+      description: "Html for Bottega site"
+    },
+    {
+        id: 8,
+        name: "John",
+        type: "finished_apps",
+        image: "images/BottegaTeam.png",
+        description: "Bottega html"
+      },
+      {
+        id: 9,
+        name: "Jackson",
+        type: "working_on",
+        image: "images/BottegaTeam.png",
+        description: "Application for apps"
+      },
+      {
+        id: 10,
+        name: "Ben",
+        type: "finished_apps",
+        image: "images/BottegaTeam.png",
+        description: "Html for Bottega site"
+      }
+  ];
---
+++ src/non-components/filters.js
@@ -0,0 +1,6 @@
+export const FILTERS = [
+  'New_apps',
+  'working_on',
+  'finished_apps',
+  'Create New Project'
+];
